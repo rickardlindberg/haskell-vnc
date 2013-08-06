1ef8752ed348c38e87191f02bae92fc933947693
Rickard Lindberg and Kajsa Goffrich <ricli85+kajgo@gmail.com>
2013-08-06 19:24:56 +0200

    Can read security types from vnc server.

--- VNC.hs
+++ VNC.hs
@@ -4,11 +4,23 @@ import Data.Char
 
 main = do
     h <- connect "localhost" 5900
+
     bytes <- readBytes 12 h
     print $ bytes
 
+    hPutStr h "RFB 003.007\n"
+
+    securityTypes <- readSecurityTypes h
+    print $ securityTypes
+
 connect :: String -> Int -> IO Handle
 connect host port = connectTo host (PortNumber (fromIntegral port))
+
+readSecurityTypes :: Handle -> IO [Int]
+readSecurityTypes h = do
+    [numberOfBytes] <- readBytes 1 h
+    securityTypes <- readBytes numberOfBytes h
+    return securityTypes
 
 readBytes :: Int -> Handle -> IO [Int]
 readBytes 0         handle = return []
