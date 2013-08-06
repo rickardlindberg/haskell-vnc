59c935047646f687e92910059d808e66b2754942
Rickard Lindberg and Kajsa Goffrich <ricli85+kajgo@gmail.com>
2013-08-06 19:58:07 +0200

    Can initialize communication.

--- VNC.hs
+++ VNC.hs
@@ -1,6 +1,7 @@
 import Network
 import System.IO
 import Data.Char
+import Data.Bits
 
 main = do
     h <- connect "localhost" 5900
@@ -19,6 +20,13 @@ main = do
     -- Use security type 1 (None)
     hPutChar h (chr 1)
 
+    -- Send init message
+    hPutChar h (chr 1)
+
+    -- Read init message
+    initMessage <- readInitMessage h
+    print $ initMessage
+
 connect :: String -> Int -> IO Handle
 connect host port = connectTo host (PortNumber (fromIntegral port))
 
@@ -29,9 +37,36 @@ readVersion h = do
 
 readSecurityTypes :: Handle -> IO [Int]
 readSecurityTypes h = do
-    [numberOfBytes] <- readBytes 1 h
+    numberOfBytes <- readU8 h
     securityTypes <- readBytes numberOfBytes h
     return securityTypes
+
+readInitMessage :: Handle -> IO (Int, Int, Int, String)
+readInitMessage h = do
+    width <- readU16 h
+    height <- readU16 h
+    pixelFormat <- readBytes 16 h
+    nameLength <- readU32 h
+    name <- readBytes nameLength h
+    return (width, height, 0, map chr name)
+
+readU8 :: Handle -> IO Int
+readU8 h = do
+    [byte] <- readBytes 1 h
+    return byte
+
+readU16 :: Handle -> IO Int
+readU16 h = do
+    [high, low] <- readBytes 2 h
+    return $ (shiftL high 8) .|. low
+
+readU32 :: Handle -> IO Int
+readU32 h = do
+    [high, highMiddle, lowMiddle, low] <- readBytes 4 h
+    return $  (shiftL high 24)
+          .|. (shiftL highMiddle 16)
+          .|. (shiftL lowMiddle 8)
+          .|. low
 
 readBytes :: Int -> Handle -> IO [Int]
 readBytes 0         handle = return []
